b74df038484c1dcc24b66e53ebdcbbb1af102153
amixOV <ovits57119@gmail.com>
2020-04-18 23:28:10 +0300

    3try fix bug h10 by not ignore img file

--- src/public/script/index.js
+++ src/public/script/index.js
@@ -12,28 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let options = {};
     let instances_side = M.Sidenav.init(ele_side, options);
 
-  
-    //console.log(instance);
-    //instance.next();
 
-    // instance.next(3);
-
-
-/*   Its done with CSS 
-
-document.addEventListener('scroll', function(e){ 
-    let footer = document.getElementById("footer_for_scroll");
-    let header = document.getElementById("header_for_scroll");
-    let panel = document.getElementById("panel_side");
-
-    let x = document.body.clientHeight - footer.offsetHeight - header.offsetHeight - window.scrollY;
-
-    panel.style.height = x + "px" ;
-
-}, true);
-*/
-
-/*
 initCArusel = () => {
     let ele_carusel = document.querySelectorAll('.carousel');
     let ele_carus = document.querySelector('#carus');
@@ -88,5 +67,20 @@ initCArusel = () => {
        
         console.log(carusel);
     }
-*/
+
 });
+
+
+/*   Its done with CSS 
+
+document.addEventListener('scroll', function(e){ 
+    let footer = document.getElementById("footer_for_scroll");
+    let header = document.getElementById("header_for_scroll");
+    let panel = document.getElementById("panel_side");
+
+    let x = document.body.clientHeight - footer.offsetHeight - header.offsetHeight - window.scrollY;
+
+    panel.style.height = x + "px" ;
+
+}, true);
+*/
